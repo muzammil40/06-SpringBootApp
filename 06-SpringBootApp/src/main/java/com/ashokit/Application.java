4a488f98ba2b139df5e49468e8d666dd2e54e42a
muzammil <smuzammil890@gmail.com>
2021-03-13 19:12:29 +0530

    HIS-300 related changes

--- 06-SpringBootApp/src/main/java/com/ashokit/Application.java
+++ 06-SpringBootApp/src/main/java/com/ashokit/Application.java
@@ -5,10 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+         Private Map<String,Object> cache= new HashMap<String,Object>();
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		int i=0;
+	}
+	public void loadDataCache(){
+		//logic
 	}
 
 }
